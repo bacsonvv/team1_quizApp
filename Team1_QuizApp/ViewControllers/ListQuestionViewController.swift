bd5bc6b75dc43d2f18a28470992bb1b51296cf68
Vuong Vu Bac Son <son.vuongvubac@V000112s-Mac-mini.local>
2020-09-09 16:41:36 +0700

    ranking screen

--- Team1_QuizApp/ViewControllers/ListQuestionViewController.swift
+++ Team1_QuizApp/ViewControllers/ListQuestionViewController.swift
@@ -99,7 +99,7 @@ class ListQuestionViewController: UIViewController {
 
 extension ListQuestionViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return listQuestion.count
+        return questionForView.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
